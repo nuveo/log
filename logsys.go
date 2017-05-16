8862b8478c37f20e7a8b31bab5be64fe4a151cc9
Cesar Gimenes <crg@crg.eti.br>
2017-05-15 21:00:10 -0300

    split the code and improves the interface

--- logsys.go
+++ logsys.go
@@ -1,26 +1,63 @@
-package logSys
+package log
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"runtime"
 	"time"
 )
 
-type MsgType uint8
+type msgType uint8
+
+// DebugMode Enable debug mode
+var DebugMode bool
 
 const (
-	Message  MsgType = 0
-	Message2 MsgType = 1
-	Warning  MsgType = 2
-	Debug    MsgType = 3
-	Error    MsgType = 0x80
+	messageConst  msgType = 0
+	message2Const msgType = 1
+	warningConst  msgType = 2
+	debugConst    msgType = 3
+	errorConst    msgType = 0x80
 )
 
-var DebugMode = false
+/*
+TODO: Create a more flexible HTTP error return as an example below
+func HTTPError(w http.ResponseWriter, code int) {
+	msg := http.StatusText(code)
+	pln(errorConst, msg)
+	m := make(map[string]string)
+	m["status"] = "error"
+	m["error"] = msg
+	b, _ := json.MarshalIndent(m, "", "\t")
+	http.Error(w, string(b), code)
+}
+*/
 
-func Println(m MsgType, msg ...interface{}) {
-	if m == Debug && !DebugMode {
+// Fatal show message with line break at the end and exit to OS.
+func Fatal(msg ...interface{}) {
+	pln(errorConst, msg...)
+	os.Exit(-1)
+}
+
+// Errorln message with line break at the end.
+func Errorln(msg ...interface{}) {
+	pln(errorConst, msg...)
+}
+
+// Println shows message on screen with line break at the end.
+func Println(msg ...interface{}) {
+	pln(messageConst, msg...)
+}
+
+// Debugln shows debug message on screen with line break at the end.
+// If debug mode is not active no message is displayed
+func Debugln(msg ...interface{}) {
+	pln(debugConst, msg...)
+}
+
+func pln(m msgType, msg ...interface{}) {
+	if m == debugConst && !DebugMode {
 		return
 	}
 
@@ -29,25 +66,25 @@ func Println(m MsgType, msg ...interface{}) {
 	var prefix string
 
 	switch m {
-	case Message:
+	case messageConst:
 		color = "\x1b[37m" // White
 		prefix = "msg"
-	case Message2:
+	case message2Const:
 		color = "\x1b[92m" // Light green
 		prefix = "msg"
-	case Warning:
+	case warningConst:
 		color = "\x1b[93m" // Light Yellow
 		prefix = "warning"
-	case Debug:
+	case debugConst:
 		color = "\x1b[96m" // Light Cyan
 		prefix = "debug"
-	case Error:
+	case errorConst:
 		color = "\x1b[91m" // Light Red
 		prefix = "error"
 	}
 
 	if DebugMode {
-		_, fn, line, _ := runtime.Caller(1)
+		_, fn, line, _ := runtime.Caller(2)
 		fn = filepath.Base(fn)
 		debugInfo = fmt.Sprintf("%s:%d ", fn, line)
 	}

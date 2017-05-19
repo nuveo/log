1fe7f3ce0d5c43ecffc774fa096d1f966fc10ff8
Cesar Gimenes <crg@crg.eti.br>
2017-05-19 10:17:02 -0300

    add templates

--- logsys.go
+++ logsys.go
@@ -1,7 +1,9 @@
 package log
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -33,6 +35,46 @@ func HTTPError(w http.ResponseWriter, code int) {
 	http.Error(w, string(b), code)
 }
 */
+
+type DebugInterface interface{}
+type LogInterface interface{}
+
+var (
+	ColorMessage  = "\x1b[37m"   // White
+	ColorMessage2 = "\x1b[92m"   // Light green
+	ColorWarning  = "\x1b[93m"   // Light Yellow
+	ColorDebug    = "\x1b[96m"   // Light Cyan
+	ColorError    = "\x1b[91m"   // Light Red
+	ColorReset    = "\033[0;00m" // Return terminal to default color
+
+	PrefixMessage  = "msg"
+	PrefixMessage2 = "msg"
+	PrefixWarinig  = "warning"
+	PrefixDebug    = "debug"
+	PrefixError    = "error"
+
+	DebugFormatter = "{{.File}}:{{.Line}}"
+	LogFormatter   = "{{.Color}}{{.Time}} [{{.Prefix}}] {{.DebugInfo}} {{.Message}}{{.ColorReset}}\n"
+
+	DebugBody DebugInterface
+	LogBody   LogInterface
+
+	/*
+		"Color":      color,
+			"Time":       time.Now().UTC().Format("2006/01/02 15:04:05"),
+			"Prefix":     prefix,
+			"DebugInfo":  debugInfo,
+			"Message":    fmt.Sprint(msg...),
+			"ColorReset": ColorReset,
+	*/
+
+	LogHandler func(LogData map[string]interface{}, Message ...string) (string, map[string]interface{}) = func(
+		LogData map[string]interface{},
+		Message ...string) (formater string, body map[string]interface{}) {
+
+		return
+	}
+)
 
 // Fatal show message with line break at the end and exit to OS.
 func Fatal(msg ...interface{}) {
@@ -67,32 +109,57 @@ func pln(m msgType, msg ...interface{}) {
 
 	switch m {
 	case messageConst:
-		color = "\x1b[37m" // White
-		prefix = "msg"
+		color = ColorMessage
+		prefix = PrefixMessage
 	case message2Const:
-		color = "\x1b[92m" // Light green
-		prefix = "msg"
+		color = ColorMessage2
+		prefix = PrefixMessage2
 	case warningConst:
-		color = "\x1b[93m" // Light Yellow
-		prefix = "warning"
+		color = ColorWarning
+		prefix = PrefixWarinig
 	case debugConst:
-		color = "\x1b[96m" // Light Cyan
-		prefix = "debug"
+		color = ColorDebug
+		prefix = PrefixDebug
 	case errorConst:
-		color = "\x1b[91m" // Light Red
-		prefix = "error"
+		color = ColorError
+		prefix = PrefixError
 	}
 
 	if DebugMode {
 		_, fn, line, _ := runtime.Caller(2)
 		fn = filepath.Base(fn)
-		debugInfo = fmt.Sprintf("%s:%d ", fn, line)
+
+		DebugBody = map[string]interface{}{
+			"File": fn,
+			"Line": line,
+		}
+
+		buff := new(bytes.Buffer)
+		t, _ := template.New("debug").Parse(DebugFormatter)
+
+		t.Execute(buff, DebugBody)
+
+		debugInfo = string(buff.Bytes())
+
 	}
 
-	fmt.Printf("%s%s [%s] %s%s\033[0;00m\n",
-		color,
-		time.Now().UTC().Format("2006/01/02 15:04:05"),
-		prefix,
-		debugInfo,
-		fmt.Sprint(msg...))
+	LogBody = map[string]interface{}{
+		"Color":      color,
+		"Time":       time.Now().UTC().Format("2006/01/02 15:04:05"),
+		"Prefix":     prefix,
+		"DebugInfo":  debugInfo,
+		"Message":    fmt.Sprint(msg...),
+		"ColorReset": ColorReset,
+	}
+
+	//runtime.Caller()
+
+	LogHandler(LogBody, message)
+
+	buff := new(bytes.Buffer)
+	t, _ := template.New("log").Parse(LogFormatter)
+
+	t.Execute(buff, LogBody)
+
+	fmt.Print(string(buff.Bytes()))
 }

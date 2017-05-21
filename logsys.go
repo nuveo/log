d720096f0dd201a686b0701bd406870d6f9ccaa8
Cesar Gimenes <crg@crg.eti.br>
2017-05-21 18:14:51 -0300

    implement lists as array

--- logsys.go
+++ logsys.go
@@ -10,65 +10,66 @@ import (
 
 type msgType uint8
 
-// DebugMode Enable debug mode
-var DebugMode bool
-
 const (
-	messageConst  msgType = 0
-	message2Const msgType = 1
-	warningConst  msgType = 2
-	debugConst    msgType = 3
-	errorConst    msgType = 0x80
+	MessageLog  msgType = 0
+	Message2Log msgType = 1
+	WarningLog  msgType = 2
+	DebugLog    msgType = 3
+	ErrorLog    msgType = 4
+
+	ColorReset = "\033[0;00m" // Return terminal to default color
+)
+
+var (
+
+	// DebugMode Enable debug mode
+	DebugMode bool
+
+	Colors = []string{
+		MessageLog:  "\x1b[37m", // White
+		Message2Log: "\x1b[92m", // Light green
+		WarningLog:  "\x1b[93m", // Light Yellow
+		DebugLog:    "\x1b[96m", // Light Cyan
+		ErrorLog:    "\x1b[91m", // Light Red
+	}
+
+	Prefixes = []string{
+		MessageLog:  "msg",
+		Message2Log: "msg",
+		WarningLog:  "warning",
+		DebugLog:    "debug",
+		ErrorLog:    "error",
+	}
 )
 
 // Fatal show message with line break at the end and exit to OS.
 func Fatal(msg ...interface{}) {
-	pln(errorConst, msg...)
+	pln(ErrorLog, msg...)
 	os.Exit(-1)
 }
 
 // Errorln message with line break at the end.
 func Errorln(msg ...interface{}) {
-	pln(errorConst, msg...)
+	pln(ErrorLog, msg...)
 }
 
 // Println shows message on screen with line break at the end.
 func Println(msg ...interface{}) {
-	pln(messageConst, msg...)
+	pln(MessageLog, msg...)
 }
 
 // Debugln shows debug message on screen with line break at the end.
 // If debug mode is not active no message is displayed
 func Debugln(msg ...interface{}) {
-	pln(debugConst, msg...)
+	pln(DebugLog, msg...)
 }
 
 func pln(m msgType, msg ...interface{}) {
-	if m == debugConst && !DebugMode {
+	if m == DebugLog && !DebugMode {
 		return
 	}
 
 	var debugInfo string
-	var color string
-	var prefix string
-
-	switch m {
-	case messageConst:
-		color = "\x1b[37m" // White
-		prefix = "msg"
-	case message2Const:
-		color = "\x1b[92m" // Light green
-		prefix = "msg"
-	case warningConst:
-		color = "\x1b[93m" // Light Yellow
-		prefix = "warning"
-	case debugConst:
-		color = "\x1b[96m" // Light Cyan
-		prefix = "debug"
-	case errorConst:
-		color = "\x1b[91m" // Light Red
-		prefix = "error"
-	}
 
 	if DebugMode {
 		_, fn, line, _ := runtime.Caller(2)
@@ -77,9 +78,9 @@ func pln(m msgType, msg ...interface{}) {
 	}
 
 	fmt.Printf("%s%s [%s] %s%s\033[0;00m\n",
-		color,
+		Colors[m],
 		time.Now().UTC().Format("2006/01/02 15:04:05"),
-		prefix,
+		Prefixes[m],
 		debugInfo,
 		fmt.Sprint(msg...))
 }

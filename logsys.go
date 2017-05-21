183701f848777517a4867a1f44248a7effdf7c78
Cesar Gimenes <crg@crg.eti.br>
2017-05-21 17:17:49 -0300

    Improves lists of prefixes and colors

--- logsys.go
+++ logsys.go
@@ -16,16 +16,18 @@ type msgType uint8
 var DebugMode bool
 
 const (
-	messageConst  msgType = 0
-	message2Const msgType = 1
-	warningConst  msgType = 2
-	debugConst    msgType = 3
-	errorConst    msgType = 0x80
+	MessageLog  msgType = 0
+	Message2Log msgType = 1
+	WarningLog  msgType = 2
+	DebugLog    msgType = 3
+	ErrorLog    msgType = 4
+
+	ColorReset = "\033[0;00m" // Return terminal to default color
+
+	DefaultLogFormatter = "{{.Color}}{{.Time}} [{{.Prefix}}] {{.File}}:{{.Line}} {{.Message}}{{.ColorReset}}\n"
 )
 
 type LogInterface map[string]interface{}
-
-const DefaultLogFormatter = "{{.Color}}{{.Time}} [{{.Prefix}}] {{.File}}:{{.Line}} {{.Message}}{{.ColorReset}}\n"
 
 func DefaultLogHandler(logData LogInterface) (string, LogInterface) {
 
@@ -43,82 +45,66 @@ func DefaultDebugLogHandler(logData LogInterface) (string, LogInterface) {
 }
 
 var (
-	ColorMessage  = "\x1b[37m"   // White
-	ColorMessage2 = "\x1b[92m"   // Light green
-	ColorWarning  = "\x1b[93m"   // Light Yellow
-	ColorDebug    = "\x1b[96m"   // Light Cyan
-	ColorError    = "\x1b[91m"   // Light Red
-	ColorReset    = "\033[0;00m" // Return terminal to default color
+	Colors = []string{
+		MessageLog:  "\x1b[37m", // White
+		Message2Log: "\x1b[92m", // Light green
+		WarningLog:  "\x1b[93m", // Light Yellow
+		DebugLog:    "\x1b[96m", // Light Cyan
+		ErrorLog:    "\x1b[91m", // Light Red
+	}
 
-	PrefixMessage  = "msg"
-	PrefixMessage2 = "msg"
-	PrefixWarinig  = "warning"
-	PrefixDebug    = "debug"
-	PrefixError    = "error"
+	Prefixes = []string{
+		MessageLog:  "msg",
+		Message2Log: "msg",
+		WarningLog:  "warning",
+		DebugLog:    "debug",
+		ErrorLog:    "error",
+	}
 
 	LogHandler func(logData LogInterface) (string, LogInterface)
 )
 
 func init() {
-	LogHandler = DefaultLogHandler
+	if LogHandler == nil {
+		LogHandler = DefaultLogHandler
+	}
 }
 
 // Fatal show message with line break at the end and exit to OS.
 func Fatal(msg ...interface{}) {
-	pln(errorConst, msg...)
+	pln(ErrorLog, msg...)
 	os.Exit(-1)
 }
 
 // Errorln message with line break at the end.
 func Errorln(msg ...interface{}) {
-	pln(errorConst, msg...)
+	pln(ErrorLog, msg...)
 }
 
 // Println shows message on screen with line break at the end.
 func Println(msg ...interface{}) {
-	pln(messageConst, msg...)
+	pln(MessageLog, msg...)
 }
 
 // Debugln shows debug message on screen with line break at the end.
 // If debug mode is not active no message is displayed
 func Debugln(msg ...interface{}) {
-	pln(debugConst, msg...)
+	pln(DebugLog, msg...)
 }
 
 func pln(m msgType, msg ...interface{}) {
-	var color string
-	var prefix string
 
-	switch m {
-	case messageConst:
-		color = ColorMessage
-		prefix = PrefixMessage
-	case message2Const:
-		color = ColorMessage2
-		prefix = PrefixMessage2
-	case warningConst:
-		color = ColorWarning
-		prefix = PrefixWarinig
-	case debugConst:
-		color = ColorDebug
-		prefix = PrefixDebug
-	case errorConst:
-		color = ColorError
-		prefix = PrefixError
-	}
-
-	var line int
-	var file string
-	_, file, line, _ = runtime.Caller(2)
+	_, file, line, _ := runtime.Caller(2)
 
 	logBody := LogInterface{
-		"Color":      color,
-		"Time":       time.Now(),
-		"Prefix":     prefix,
-		"Message":    fmt.Sprint(msg...),
+		"Color":      Colors[m],
 		"ColorReset": ColorReset,
-		"Line":       line,
 		"File":       file,
+		"Line":       line,
+		"Message":    fmt.Sprint(msg...),
+		"MsgType":    m,
+		"Prefix":     Prefixes[m],
+		"Time":       time.Now(),
 	}
 
 	var logFormater string

a19e9bd8024af72d821085826eff2d60e03c765e
Cesar Gimenes <crg@crg.eti.br>
2017-05-25 18:44:33 -0300

    add benchmark

---
+++ logsys_test.go
@@ -0,0 +1,11 @@
+package log
+
+import "testing"
+
+func BenchmarkHello(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Println("test log message")
+		Errorln("test log message")
+		Debugln("test log message")
+	}
+}

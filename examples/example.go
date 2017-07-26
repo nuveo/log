944be838f3dea557868aa61b3da62e78dbac746e
Ervilis Souza <ervilisviana@gmail.com>
2017-07-26 12:52:04 -0300

    Adding example program

---
+++ examples/example.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nuveo/logSys"
+)
+
+func main() {
+	log.Debugln("Debug message")
+
+	log.DebugMode = false
+	log.Debugln("Debug message that will be hidden")
+
+	log.Println("Info message")
+	log.Warningln("Warning message")
+	log.Errorln("Error message")
+	log.Fatal("Fatal error message")
+	fmt.Println("I will never be printed because of Fatal()")
+}
